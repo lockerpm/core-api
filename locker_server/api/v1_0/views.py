b9759060fdbbd0b3155d4b67439a38692a74eb4e
Khai Tran <khaitq@cystack.net>
2023-10-02 16:10:48 +0700

    chore: Fix exclude domain subdomain

--- locker_server/api/v1_0/views.py
+++ locker_server/api/v1_0/views.py
@@ -1,2 +1,6 @@
 from locker_server.api.v1_0.resources.views import ResourcePwdViewSet
 from locker_server.api.v1_0.notification_settings.views import NotificationSettingPwdViewSet
+
+from locker_server.api.v1_0.tools.views import ToolPwdViewSet
+
+from locker_server.api.v1_0.exclude_domains.views import ExcludeDomainPwdViewSet
