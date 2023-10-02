63df36dc3d763f519deeef27eecedca2218d92d2
Khai Tran <khaitq@cystack.net>
2023-10-02 14:40:35 +0700

    fix: Fix migrations

--- locker_server/api_orm/models/users/user_score.py
+++ locker_server/api_orm/models/users/user_score.py
@@ -4,3 +4,9 @@ from locker_server.api_orm.abstracts.users.user_score import AbstractUserScoreOR
 class UserScoreORM(AbstractUserScoreORM):
     class Meta(AbstractUserScoreORM.Meta):
         db_table = 'cs_user_score'
+
+    @classmethod
+    def create(cls, user):
+        new_bw_user_score = cls(user=user)
+        new_bw_user_score.save()
+        return new_bw_user_score
