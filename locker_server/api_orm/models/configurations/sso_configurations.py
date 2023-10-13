06a7af66beb764b1b88f0c156dd3200db90eac18
Phương Nguyễn Thị Thu <phuongntt@cystack.net>
2023-10-13 14:01:35 +0700

    chore: Change SSOConfiguration api path

--- locker_server/api_orm/models/configurations/sso_configurations.py
+++ locker_server/api_orm/models/configurations/sso_configurations.py
@@ -1,3 +1,5 @@
+from django.db import transaction
+
 from locker_server.api_orm.abstracts.configurations.sso_configurations import AbstractSSOConfigurationORM
 from locker_server.shared.utils.app import now
 
@@ -9,13 +11,17 @@ class SSOConfigurationORM(AbstractSSOConfigurationORM):
 
     @classmethod
     def create(cls, **data):
-        sso_config_orm = cls(
-            created_by_id=data.get("created_by_id"),
-            sso_provider_id=data.get("sso_provider_id"),
-            identifier=data.get("identifier"),
-            enabled=data.get("enabled"),
-            sso_provider_options=data.get("sso_provider_options"),
-            creation_date=data.get("creation_date", now())
-        )
-        sso_config_orm.save()
-        return sso_config_orm
+        with transaction.atomic():
+            try:
+                sso_config_orm = cls.objects.get()
+            except cls.DoesNotExist:
+                sso_config_orm = cls(
+                    created_by_id=data.get("created_by_id"),
+                    sso_provider_id=data.get("sso_provider_id"),
+                    identifier=data.get("identifier"),
+                    enabled=data.get("enabled"),
+                    sso_provider_options=data.get("sso_provider_options"),
+                    creation_date=data.get("creation_date", now())
+                )
+                sso_config_orm.save()
+            return sso_config_orm
