8eaaa8dbafefec614f58d4573bd622f7e0fc36a8
Phương Nguyễn Thị Thu <phuongntt@cystack.net>
2023-10-18 10:28:14 +0700

    chore: Fix cron task

--- locker_server/cron/utils/django_config.py
+++ locker_server/cron/utils/django_config.py
@@ -11,17 +11,14 @@ from locker_server.shared.middlewares.tenant_db_middleware import set_current_db
 
 def django_config(db_name='default'):
     try:
+        env_file = os.path.join(Path(__file__).resolve().parent.parent.parent.parent, '.env')
+        dotenv.read_dotenv(env_file)
         valid_env = ['prod', 'env', 'staging']
         env = os.getenv("PROD_ENV")
         if env not in valid_env:
             env = 'dev'
-        if env == "dev":
-            dotenv.read_dotenv(os.path.join(Path(__file__).resolve().parent.parent.parent, '.env'))
         setting = "server_config.settings.%s" % env
-        # logger.info(f"[+] Start cron by setting {setting}")
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", setting)
-
-        set_current_db_name(db_name)
         django.setup()
     except:
         tb = traceback.format_exc()
