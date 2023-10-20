2bf139baeaca79867bc8ca6944c39e45dd757b4d
Khai Tran <khaitq@cystack.net>
2023-10-20 18:52:26 +0700

    chore: Update

--- locker_server/api_orm/repositories/team_group_repository.py
+++ locker_server/api_orm/repositories/team_group_repository.py
@@ -59,7 +59,7 @@ class TeamGroupORMRepository(TeamGroupRepository):
 
     def get_share_group_by_enterprise_group_id(self, sharing_id: str, enterprise_group_id: str) -> Optional[Group]:
         try:
-            group_orm = GroupORM.objects.get(identerprise_group_id=enterprise_group_id, team_id=sharing_id)
+            group_orm = GroupORM.objects.get(enterprise_group_id=enterprise_group_id, team_id=sharing_id)
         except GroupORM.DoesNotExist:
             return None
         return ModelParser.team_parser().parse_group(group_orm=group_orm)
