6f0572fd1b650c69d5008ba452df317873a7bcfe
Khai Tran <khaitq@cystack.net>
2023-10-20 16:10:08 +0700

    chore: Update logs

--- locker_server/shared/log/handlers.py
+++ locker_server/shared/log/handlers.py
@@ -9,11 +9,30 @@ class SlackHandler(logging.Handler):
         try:
             record.exc_info = record.exc_text = None
             content = {'text': self.format(record)}
-            # TODO authorize to thread job
             requests.post(
                 url=settings.SLACK_WEBHOOK_API_LOG,
                 json={"text": "```{}```".format(content['text'])},
                 timeout=10
             )
         except Exception:
+            print("Slack handler failed")
+
+
+class SlackNewUserHandler(logging.Handler):
+    def emit(self, record):
+        try:
+            record.exc_info = record.exc_text = None
+            slack_attachments = [{
+                "fallback": self.format(record),
+                "color": "#0476E9",
+                "text": record.msg,
+            }]
+            requests.post(
+                url=settings.SLACK_WEBHOOK_NEW_USERS,
+                json={
+                    "attachments": slack_attachments
+                },
+                timeout=10
+            )
+        except:
             print("Slack handler failed")
