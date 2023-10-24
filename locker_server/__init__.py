cf8d2e67ebda5c2da87e493cbd8bae4d5c79e758
Phương Nguyễn Thị Thu <phuongntt@cystack.net>
2023-10-24 15:32:30 +0700

    chore: Handle deadlock

---
+++ locker_server/__init__.py
@@ -0,0 +1,24 @@
+import django.db.backends.utils
+from django.db import OperationalError
+import time
+
+from locker_server.shared.log.cylog import CyLog
+
+original = django.db.backends.utils.CursorWrapper.execute
+
+
+def execute_wrapper(*args, **kwargs):
+    attempts = 0
+    CyLog.error(**{"message": "[DATABASE] error, try restart query"})
+    while attempts < 3:
+        try:
+            return original(*args, **kwargs)
+        except OperationalError as e:
+            code = e.args[0]
+            if attempts == 2 or code != 1213:
+                raise e
+            attempts += 1
+            time.sleep(0.2)
+
+
+django.db.backends.utils.CursorWrapper.execute = execute_wrapper
