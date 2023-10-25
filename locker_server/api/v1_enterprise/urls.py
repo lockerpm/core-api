97461feff4ad413d4fcf302a002533854ec83a8b
Phương Nguyễn Thị Thu <phuongntt@cystack.net>
2023-10-25 13:38:44 +0700

    chore: Remove enterprise domain urls

--- locker_server/api/v1_enterprise/urls.py
+++ locker_server/api/v1_enterprise/urls.py
@@ -10,15 +10,6 @@ urlpatterns = [
     url(r'^', include(router.urls))
 ]
 
-# ----------------------------------- Domain ------------------------- #
-urlpatterns += [
-    url(r'^(?P<pk>[0-9a-z]+)/domains$', views.DomainPwdViewSet.as_view({'get': 'list', 'post': 'create'})),
-    url(r'^(?P<pk>[0-9a-z]+)/domains/(?P<domain_id>[0-9]+)$',
-        views.DomainPwdViewSet.as_view({'put': 'update', 'delete': 'destroy'})),
-    url(r'^(?P<pk>[0-9a-z]+)/domains/(?P<domain_id>[0-9]+)/verification$',
-        views.DomainPwdViewSet.as_view({'get': 'verification', 'post': 'verification'})),
-]
-
 # ----------------------------------- Members ------------------------- #
 urlpatterns += [
     url(r'^(?P<pk>[0-9a-z]+)/members$', views.MemberPwdViewSet.as_view({'get': 'list'})),
