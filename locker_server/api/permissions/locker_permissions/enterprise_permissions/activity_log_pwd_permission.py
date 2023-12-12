46209c21c487821a6e2673fe615ad2ba6f527263
phương nguyễn thị thu <phuongntt@cystack.net>
2023-12-12 18:07:36 +0700

    chore: Update ActivityLogPwdPermission

--- locker_server/api/permissions/locker_permissions/enterprise_permissions/activity_log_pwd_permission.py
+++ locker_server/api/permissions/locker_permissions/enterprise_permissions/activity_log_pwd_permission.py
@@ -1,6 +1,7 @@
 from locker_server.api.permissions.locker_permissions.enterprise_permissions.enterprise_pwd_permission import \
     EnterprisePwdPermission
-from locker_server.shared.constants.enterprise_members import E_MEMBER_ROLE_PRIMARY_ADMIN, E_MEMBER_ROLE_ADMIN
+from locker_server.shared.constants.enterprise_members import E_MEMBER_ROLE_PRIMARY_ADMIN, E_MEMBER_ROLE_ADMIN, \
+    E_MEMBER_ROLE_MEMBER
 
 
 class ActivityLogPwdPermission(EnterprisePwdPermission):
@@ -15,4 +16,6 @@ class ActivityLogPwdPermission(EnterprisePwdPermission):
         member = self.get_enterprise_member(user=request.user, obj=obj)
         role = member.role
         role_name = role.name
+        if view.action in ['list']:
+            return role_name in [E_MEMBER_ROLE_PRIMARY_ADMIN, E_MEMBER_ROLE_ADMIN, E_MEMBER_ROLE_MEMBER]
         return role_name in [E_MEMBER_ROLE_PRIMARY_ADMIN, E_MEMBER_ROLE_ADMIN]
