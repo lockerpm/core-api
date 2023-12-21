31c5d7a2ebf4e51c1d2e38f31b94ec3f6ddc22a3
phuongntt <phuongntt@cystack.net>
2023-12-21 13:16:33 +0700

    chore: Update default value for credential type

--- locker_server/api/v1_0/passwordless/serializers.py
+++ locker_server/api/v1_0/passwordless/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 
-from locker_server.shared.constants.backup_credential import LIST_CREDENTIAL_TYPE
+from locker_server.shared.constants.backup_credential import LIST_CREDENTIAL_TYPE, CREDENTIAL_TYPE_HMAC
 
 
 class PasswordlessCredentialSerializer(serializers.Serializer):
     credential_id = serializers.CharField(max_length=255)
     name = serializers.CharField(max_length=255, allow_blank=False)
-    type = serializers.ChoiceField(choices=LIST_CREDENTIAL_TYPE, required=False)
+    type = serializers.ChoiceField(choices=LIST_CREDENTIAL_TYPE, required=False, default=CREDENTIAL_TYPE_HMAC)
