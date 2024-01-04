acd0dab7867f5fd16b1aad4ef42c5bcf2c0be942
Khai Tran <khaitq@cystack.net>
2024-01-04 14:24:21 +0700

    chore: Update hook permission

--- locker_server/api/permissions/micro_service_permissions/payment_permission.py
+++ locker_server/api/permissions/micro_service_permissions/payment_permission.py
@@ -3,7 +3,7 @@ from locker_server.api.permissions.app import APIPermission
 
 class PaymentPermission(APIPermission):
     def has_permission(self, request, view):
-        if view.action in ["webhook_create", "webhook_set_status",
+        if view.action in ["webhook_create", "webhook_set_status", "webhook_refunded",
                            "webhook_unpaid_subscription", "webhook_cancel_subscription",
                            "banking_callback", "referral_payment",
                            "charge_set_status"]:
@@ -11,7 +11,7 @@ class PaymentPermission(APIPermission):
         return super(PaymentPermission, self).has_permission(request, view)
 
     def has_object_permission(self, request, view, obj):
-        if view.action in ["webhook_create", "webhook_set_status",
+        if view.action in ["webhook_create", "webhook_set_status", "webhook_refunded", 
                            "webhook_unpaid_subscription", "webhook_cancel_subscription",
                            "banking_callback"]:
             return True
