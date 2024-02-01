8d97a07d57b8deed419579c7a9767feb59d1b4b3
Khai Tran <khaitq@cystack.net>
2024-02-01 11:15:03 +0700

    chore: Update urls

--- locker_server/api/v1_0/urls.py
+++ locker_server/api/v1_0/urls.py
@@ -85,6 +85,10 @@ urlpatterns += [
     url(r'^notification/settings$', views.NotificationSettingPwdViewSet.as_view({'get': 'list'})),
     url(r'^notification/settings/(?P<category_id>[a-z_]+)$',
         views.NotificationSettingPwdViewSet.as_view({'put': 'update'})),
+    # TODO: Remove them later
+    url(r'^notifcation/settings$', views.NotificationSettingPwdViewSet.as_view({'get': 'list'})),
+    url(r'^notifcation/settings/(?P<category_id>[a-z_]+)$',
+        views.NotificationSettingPwdViewSet.as_view({'put': 'update'})),
 ]
 
 # -------------------------------- Sync ----------------------------------- #
