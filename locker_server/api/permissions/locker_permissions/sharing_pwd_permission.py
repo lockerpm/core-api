1d1a86d2f0a412ea77cd242e93402943f9d0633a
Khai Tran <khaitq@cystack.net>
2026-07-25 17:20:56 +0700

    chore: Update permissions

--- locker_server/api/permissions/locker_permissions/sharing_pwd_permission.py
+++ locker_server/api/permissions/locker_permissions/sharing_pwd_permission.py
@@ -1,12 +1,20 @@
+from rest_framework.exceptions import ValidationError
+
 from locker_server.api.permissions.app import APIPermission
 from locker_server.shared.constants.members import *
+from locker_server.shared.error_responses.error import gen_error
 
 
 class SharingPwdPermission(APIPermission):
     # scope = 'cipher'
 
     def has_permission(self, request, view):
-        return self.is_auth(request) and request.user.activated
+        is_authenticated = self.is_auth(request) and request.user.activated
+        if not is_authenticated:
+            return False
+        if self.is_locked_by_enterprise(user=request.user):
+            raise ValidationError({"non_field_errors": [gen_error("1009")]})
+        return True
 
     def has_object_permission(self, request, view, obj):
         if view.action in ["public_key"]:
